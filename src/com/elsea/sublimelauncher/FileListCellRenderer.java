cc29db5ac7fe410a36a375d8aa05a09baec41723
Connorelsea <connorelsea@gmail.com>
2015-12-15 02:44:23 -0600

    Optimized project panel rendering

--- src/com/elsea/sublimelauncher/FileListCellRenderer.java
+++ src/com/elsea/sublimelauncher/FileListCellRenderer.java
@@ -1,28 +1,8 @@
 package com.elsea.sublimelauncher;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Insets;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
-import javax.swing.border.Border;
-import javax.swing.border.LineBorder;
-import javax.swing.text.Position.Bias;
 
 public class FileListCellRenderer implements ListCellRenderer {
 
@@ -36,69 +16,7 @@ public class FileListCellRenderer implements ListCellRenderer {
 	) {
 		
 		Project project = (Project) value;
-		
-		// Container Panel
-		
-		JPanel container = new JPanel();
-		container.setLayout(new BorderLayout());
-		
-		
-		
-		// Name Panel
-		
-		JPanel panel_right = new JPanel();
-		panel_right.setLayout(new BoxLayout(panel_right, BoxLayout.Y_AXIS));
-		panel_right.setBackground(new Color(234, 241, 251));
-		
-		JLabel label = new JLabel(project.getName());
-		label.setForeground(new Color(25, 50, 95));
-		label.setFont(new Font("Sans Serif", Font.PLAIN, 17));
-		
-		JLabel path = new JLabel(project.getLocation().getAbsolutePath());
-		path.setForeground(new Color(25, 50, 95));
-		
-		panel_right.add(Box.createRigidArea(new Dimension(30, 10)));
-		panel_right.add(label);
-		panel_right.add(Box.createRigidArea(new Dimension(5, 5)));
-		panel_right.add(path);
-		panel_right.add(Box.createRigidArea(new Dimension(30, 10)));
-		
-		container.add(panel_right, BorderLayout.CENTER);
-		if (selected) panel_right.setBackground(new Color(207, 220, 241));
-		
-		// Get Icon
-		
-		if (project.hasIcon()) {
-			
-			System.out.println("here");
-			
-			// Icon Panel
-			
-			JPanel panel_left = new JPanel() {
-				public void paint(Graphics g) {
-					
-					super.paint(g);
-					
-					int x = (60 / 2) - (50 / 2);
-					int y = (panel_right.getHeight() / 2) - (50 / 2);
-					g.drawImage(project.getBufferedIcon(), x, y, 50, 50, null);
-					
-				}
-			};
-			
-			panel_left.setBackground(new Color(234, 241, 251));
-			
-			int height = panel_right.getHeight();
-			panel_left.setPreferredSize(new Dimension(60, height));
-			panel_left.setMinimumSize(new Dimension(60, height));
-			panel_left.setMaximumSize(new Dimension(60, height));
-			
-			container.add(panel_left, BorderLayout.WEST);
-			
-			if (selected) panel_left.setBackground(new Color(207, 220, 241));
-		}
-		
-		return container;
+		return project.getPanel();
 	}
 
 }
--- src/com/elsea/sublimelauncher/Project.java
+++ src/com/elsea/sublimelauncher/Project.java
@@ -1,10 +1,20 @@
 package com.elsea.sublimelauncher;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JApplet;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Project {
 	
@@ -14,6 +24,8 @@ public class Project {
 	private BufferedImage bufferedIcon;
 	private File   		  icon;
 	private boolean       hasIcon;
+	
+	private JPanel panel;
 	
 	public Project(String name, String path, String iconPath) {
 		
@@ -41,6 +53,65 @@ public class Project {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public JPanel getPanel() {
+		
+		if (panel == null) {
+			
+			Dimension container  = new Dimension(300, 60);
+			Color     background = new Color(234, 241, 251);
+			Color     foreground = new Color(25, 50, 95);
+			
+			panel = new JPanel();
+			panel.setLayout(new BorderLayout());
+			panel.setBackground(background);
+			panel.setPreferredSize(container);
+			panel.setMinimumSize(container);
+			panel.setMaximumSize(container);
+			
+			JPanel panelTitle = new JPanel();
+			panelTitle.setBackground(background);
+			panelTitle.setLayout(new BoxLayout(panelTitle, BoxLayout.Y_AXIS));
+			
+			JLabel labelName = new JLabel(getName());
+			JLabel labelPath = new JLabel(getLocation().getAbsolutePath());
+			
+			labelName.setForeground(foreground);
+			labelPath.setForeground(foreground);
+			
+			panelTitle.add(labelName);
+			panelTitle.add(labelPath);
+			
+			panel.add(panelTitle, BorderLayout.CENTER);
+			
+			if (hasIcon()) {
+				
+				Dimension dimIcon = new Dimension(60, 60);
+				
+				JPanel panelIcon = new JPanel() {
+					
+					public void paint(Graphics g) {
+						Graphics2D g2d = (Graphics2D) g;
+						
+						int x = (60 / 2) - (50 / 2);
+						int y = (60 / 2) - (50 / 2);
+						g.drawImage(getBufferedIcon(), x, y, 50, 50, null);
+					}
+					
+				};
+				
+				panelIcon.setBackground(background);
+				panelIcon.setPreferredSize(dimIcon);
+				panelIcon.setMinimumSize(dimIcon);
+				panelIcon.setMaximumSize(dimIcon);
+				
+				panel.add(panelIcon, BorderLayout.WEST);
+			}
+			
+		}
+		
+		return panel;
 	}
 	
 	public BufferedImage getBufferedIcon() {
